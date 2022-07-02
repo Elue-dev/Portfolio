30fff1a9c9c91ec5759d54fedeeef3db0a39e81b
Wisdom Elue <mac@Wisdoms-MacBook-Pro.local>
2022-07-02 02:53:51 +0100

    commit

--- src/components/projects/projectsdata.js
+++ src/components/projects/projectsdata.js
@@ -1,6 +1,6 @@
 export const projects = [
     {
-        id: '1',
+        id: 1,
         image: 'wworld.png',
         title: 'Wisdoms World Blog',
         desc: 'A public health platform aimed at sharing common diseases and preventive tips, health quotes etc',
@@ -8,7 +8,7 @@ export const projects = [
         stacks: 'html, css, vanilla javaScript'
     },
     {
-        id: '2',
+        id: 2,
         image: 'quickshop.png',
         title: 'Ecommerce Application',
         desc: 'A e-commerce app built with react, firebase for authentication and flutterwave as payment gateway',
@@ -16,23 +16,23 @@ export const projects = [
         stacks: 'react w/ context API & useReducers, firebase'
     },
     {
-        id: '3',
+        id: 3,
         image: 'abnb.png',
         title: 'AirBnB like clone',
         desc: 'AirBnB like web app built with React and Redux. JSON Web Token (from a backend dev) for user verification.',
         href: 'https://abnb.netlify.app',
         stacks: 'react, redux, css'
     },
-    // {
-    //     id: '4',
-    //     image: 'shophub.png',
-    //     title: 'React Shopping Cart',
-    //     desc: 'A simple react shopping cart built using Context for state management with useReducer hooks.',
-    //     href: 'https://shophubapp.netlify.app',
-    //     stacks: 'react, w/ context API, css'
-    // },
     {
-        id: '5',
+        id: 4,
+        image: 'fintracker.png',
+        title: 'Finance tracker app',
+        desc: 'Simple react app where you can store and track your transactions. The transactions that shows is dependent on the user that is signed in. Homepage is a protected route as well.',
+        href: 'https://financetrackerapp.netlify.app',
+        stacks: 'react w/ context API, firebase'
+    },
+    {
+        id: 5,
         image: 'auth.png',
         title: ' React Authentication',
         desc: 'React authentication system with firebase, includes signup, login, google OAuth, forgot password features.',
@@ -40,7 +40,7 @@ export const projects = [
         stacks: 'react w/ context API, firebase'
     },
     {
-        id: '6',
+        id: 6,
         image: 'rdirectory.png',
         title: 'Recipe app with firebase',
         desc: 'Simple app showing a recipe directory where one can add recipes only if they have an account. Other nice features added',
@@ -48,7 +48,7 @@ export const projects = [
         stacks: 'react w/ context API, firebase'
     },
     {
-        id: '7',
+        id: 7,
         image: 'msearch.png',
         title: 'Movie Searching App',
         desc: 'React application where any movie can be searched, added to favourites and stored in local storage.',
@@ -56,7 +56,7 @@ export const projects = [
         stacks: 'react, css'
     },
     {
-        id: '8',
+        id: 8,
         image: 'boon.png',
         title: 'Resort landing page',
         desc: 'A travel resort landing page, showcasing awesome resorts where people can come to relax or have vacations.',
@@ -64,15 +64,15 @@ export const projects = [
         stacks: 'html, css, vanilla javaScript'
     },
     {
-        id: '9',
+        id: 9,
         image: 'rmorty.png',
         title: 'Rick and Morty App',
         desc: 'Rick and Morty App built with React and GraphQL',
-        href: 'https://ricknmorty.netlify.app',
+        href: 'https://ricknmortyapp.netlify.app',
         stacks: 'react , css, graphQL'
     },
     {
-        id: '10',
+        id: 10,
         image: 'microsoft.png',
         title: 'Microsoft Homepage  Clone',
         desc: 'AirBnB like web app built with React and Redux. JSON Web Token (from a backend dev) for user verification.',
@@ -80,7 +80,7 @@ export const projects = [
         stacks: 'html, css, vanilla javaScript'
     },
     {
-        id: '11',
+        id: 11,
         image: 'ecommerce.png',
         title: 'Ecommerce website design',
         desc: 'Frontend of a mini e-commerce site. ',
@@ -88,7 +88,7 @@ export const projects = [
         stacks: 'html , css, vanilla javaScript'
     },
     {
-        id: '12',
+        id: 12,
         image: 'numguess.png',
         title: 'Number guesser game',
         desc: 'A Vanilla JavaScript application where random numbers are guessed until it matches the programmed number.',
@@ -96,7 +96,7 @@ export const projects = [
         stacks: 'html, css, vanilla JavaScript'
     },
     {
-        id: '13',
+        id: 13,
         image: 'gpt3.png',
         title: 'GPT-3 Website',
         desc: 'A very cool React landing page built with common react method eg props, state, mapping, etc',
