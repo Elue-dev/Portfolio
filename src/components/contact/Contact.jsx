8edd45a22e6ba939c0ab1b393993b25e161e1e14
Wisdom Elue <mac@Wisdoms-MacBook-Pro.local>
2022-07-14 05:19:03 +0100

    commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,102 +1,164 @@
-import { useState } from 'react'
-import './Contact.scss'
-import { RiContactsFill } from 'react-icons/ri'
-import { ImLocation } from 'react-icons/im'
-import { GiLetterBomb } from 'react-icons/gi'
-import { AiOutlineLinkedin } from 'react-icons/ai'
-import { BsGithub } from 'react-icons/bs'
-import { AiFillFileText } from 'react-icons/ai'
-import { BsChatRightText, BsCheck2All } from 'react-icons/bs'
-import { FiAlertTriangle } from 'react-icons/fi'
-import { BiLoader } from 'react-icons/bi'
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
-import { database } from '../../firebase'
+import { useState } from "react";
+import "./Contact.scss";
+import { RiContactsFill } from "react-icons/ri";
+import { ImLocation } from "react-icons/im";
+import { GiLetterBomb } from "react-icons/gi";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
+import { AiFillFileText } from "react-icons/ai";
+import { BsChatRightText, BsCheck2All } from "react-icons/bs";
+import { FiAlertTriangle } from "react-icons/fi";
+import { BiLoader } from "react-icons/bi";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { database } from "../../firebase";
+import ReactWhatsapp from "react-whatsapp";
+import { TbBrandWhatsapp } from "react-icons/tb";
 
 export default function Contact() {
-    const [title, setTitle] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [error, setError] = useState(null)
-    const [success, setSuccess] = useState(null)
-    const [loading, setLoading] = useState(null)
-    
-    const handleSubmit = async(e) => {
-        e.preventDefault()
+  const [title, setTitle] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-        try {
-            if (title === '' || email === '' || message === '') {
-              setError('Please fill all fields')
-              window.setTimeout(() => {
-                setError('')
-            }, 3000)
-              return
-            }
-      
-            setLoading(true)
-            setError('')
-            setSuccess('')
-      
-            const colRef = collection(database, 'messages')
-            await addDoc(colRef, {
-              title, email, message,
-              time: serverTimestamp()
-            })
-            setLoading(false)
-            setSuccess('Your message has been recieved, thank you')
-            window.setTimeout(() => {
-              setSuccess('')
-          }, 4000)
-              setTitle('')
-              setEmail('')
-              setMessage('')
-          } catch (err) {
-            console.log(err.message)
-          }
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (title === "" || email === "" || message === "") {
+        setError("Please fill all fields");
+        window.setTimeout(() => {
+          setError("");
+        }, 3000);
+        return;
+      }
+
+      setLoading(true);
+      setError("");
+      setSuccess("");
+
+      const colRef = collection(database, "messages");
+      await addDoc(colRef, {
+        title,
+        email,
+        message,
+        time: serverTimestamp(),
+      });
+      setLoading(false);
+      setSuccess("Your message has been recieved, thank you");
+      window.setTimeout(() => {
+        setSuccess("");
+      }, 4000);
+      setTitle("");
+      setEmail("");
+      setMessage("");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
-    <div className='contact' id='contact'>
-        <div className="title">Contacts</div>
-        <div className="contact_items">
-            <div className="contact_details">
-                <div className="info">
-                    <div className="name">
-                        <RiContactsFill className='contact_icon' />
-                        <p>Wisdom Elue</p>
-                    </div>
-                    <div className="location">
-                        <ImLocation className='contact_icon' />
-                        <p>Lagos, Nigeria</p>
-                    </div>
-                    <div className="email">
-                        <GiLetterBomb className='contact_icon' />
-                        <a href="mailto:eluewisdom@gmail.com">eluewisdom@gmail.com</a>
-                    </div>
-                    <div className='linkedin'>
-                        <AiOutlineLinkedin className='contact_icon' />
-                        <a href="https://www.linkedin.com/in/wisdom-elue-8822a5188">LinkedIn</a>
-                    </div>
-                    <div className="github">
-                        <BsGithub className='contact_icon' />
-                        <a href='https://github.com/Elue-dev' >github.com/Elue-dev</a>
-                    </div>
-                    <div className='resume'>
-                        <AiFillFileText className='contact_icon' />
-                        <a href="https://eluedevresume.netlify.app"> Resume</a>
-                    </div>
-                </div>
+    <div className="contact" id="contact">
+      <div className="title">Contacts</div>
+      <div className="contact_items">
+        <div className="contact_details">
+          <div className="info">
+            <div className="name">
+              <RiContactsFill className="contact_icon" />
+              <p>Wisdom Elue</p>
             </div>
-            <form className="contact_form" onSubmit={handleSubmit} name='contact-form' data-netlify='true'>
-                <h3 className="heading">Leave a Message.</h3>
-                {error && <p className='error'> <span><FiAlertTriangle />&nbsp; {error}</span></p>}
-                {success && <p className='success'> <span><BsCheck2All/>&nbsp; {success}</span></p>}
-                <input type="hidden" name='form-name' value='contact-form' />
-                <RiContactsFill className='form_icon '/><input type="text" name='name' placeholder='Your Name...' value={title} onChange={(e)=>setTitle(e.target.value)} /><br />
-                <GiLetterBomb className='form_icon' /><input type="email" name='email' placeholder=' Your Email...' value={email} onChange={(e)=>setEmail(e.target.value)} /><br />
-                <BsChatRightText className='form_icon' /><input name='message' placeholder= 'Message...' value={message} onChange={(e)=>setMessage(e.target.value)} /><br />
-                <button type='sumbit' className='submit'>{loading ? <BiLoader /> : 'Submit'}</button>
-            </form>
+            <div className="location">
+              <ImLocation className="contact_icon" />
+              <p>Lagos, Nigeria</p>
+            </div>
+            <div className="email">
+              <GiLetterBomb className="contact_icon" />
+              <a href="mailto:eluewisdom@gmail.com">eluewisdom@gmail.com</a>
+            </div>
+            <div className="linkedin">
+              <AiOutlineLinkedin className="contact_icon" />
+              <a href="https://www.linkedin.com/in/wisdom-elue-8822a5188">
+                LinkedIn
+              </a>
+            </div>
+            <div className="github">
+              <BsGithub className="contact_icon" />
+              <a href="https://github.com/Elue-dev">github.com/Elue-dev</a>
+            </div>
+            <div className="resume">
+              <AiFillFileText className="contact_icon" />
+              <a href="https://eluedevresume.netlify.app"> Resume</a>
+            </div>
+            <div>
+              <ReactWhatsapp
+                number="234-810-733-9039"
+                message="Hi there, i am from your website, i want to leave a message..."
+                className="whatsapp"
+              >
+                <TbBrandWhatsapp size={25} /> 
+                &nbsp; <span>Leave a message</span>
+              </ReactWhatsapp>
+            </div>
+          </div>
         </div>
+        <form
+          className="contact_form"
+          onSubmit={handleSubmit}
+          name="contact-form"
+          data-netlify="true"
+        >
+          <h3 className="heading">Leave a Message.</h3>
+          {error && (
+            <p className="error">
+              {" "}
+              <span>
+                <FiAlertTriangle />
+                &nbsp; {error}
+              </span>
+            </p>
+          )}
+          {success && (
+            <p className="success">
+              {" "}
+              <span>
+                <BsCheck2All />
+                &nbsp; {success}
+              </span>
+            </p>
+          )}
+          <input type="hidden" name="form-name" value="contact-form" />
+          <RiContactsFill className="form_icon " />
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <br />
+          <GiLetterBomb className="form_icon" />
+          <input
+            type="email"
+            name="email"
+            placeholder=" Your Email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <BsChatRightText className="form_icon" />
+          <input
+            name="message"
+            placeholder="Message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <br />
+          <button type="sumbit" className="submit">
+            {loading ? <BiLoader /> : "Submit"}
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,73 +1,108 @@
-import { useContext, useState } from 'react'
-import './Navbar.scss'
-import { useAuth } from '../../contexts/AuthContext'
-import { RiMenuAddLine } from 'react-icons/ri'
-import { VscEyeClosed } from 'react-icons/vsc'
-import { MdOutlineLightMode, MdLightMode } from 'react-icons/md'
-import { ModeContext } from '../../contexts/ModeContext'
+import { useContext, useState } from "react";
+import "./Navbar.scss";
+import { useAuth } from "../../contexts/AuthContext";
+import { RiMenuAddLine } from "react-icons/ri";
+import { VscEyeClosed } from "react-icons/vsc";
+import { MdOutlineLightMode, MdLightMode } from "react-icons/md";
+import { ModeContext } from "../../contexts/ModeContext";
 
 export default function Navbar() {
-  const { user, googleSignin, logout } = useAuth()
-  const [menuOpen, setMenuOpen] = useState(false)
-  const { mode, changeMode } = useContext(ModeContext)
+  const { user, googleSignin, logout } = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { mode, changeMode } = useContext(ModeContext);
 
   const handleGoogleSignIn = async () => {
     try {
-      await googleSignin()
-      setMenuOpen(false)
-    } catch(err) {
-      console.log(err.message)
+      await googleSignin();
+      setMenuOpen(false);
+    } catch (err) {
+      console.log(err.message);
     }
-  }
+  };
 
   const handleLogout = async () => {
     try {
-      await logout()
-      setMenuOpen(false)
-    } catch(err) {
-      console.log(err.message)
+      await logout();
+      setMenuOpen(false);
+    } catch (err) {
+      console.log(err.message);
     }
-  }
+  };
 
   return (
     <nav>
       <div className="nav">
         <div className="logo">
-            <a href='#'>Elue-<span>Dev</span></a>
+          <a href="#">
+            Elue-<span>Dev</span>
+          </a>
         </div>
         <div className="menu">
-          {menuOpen ? <VscEyeClosed onClick={() => setMenuOpen(!menuOpen)} className='menu_open' /> :
-            <RiMenuAddLine onClick={() => setMenuOpen(!menuOpen)} className='menu_open' />
-           }
-        </div> 
-        <ul className={menuOpen ? 'nav_links show' : 'nav_links'} onClick={()=>setMenuOpen(false)}>
-            <li ><a href="#">Home</a></li>
-            <li ><a href="#about">About</a></li>
-            <li ><a href="#skills">Skills</a></li>
-            <li ><a href="#projects">Projects</a></li>
-            <li ><a href="#testimonials">Testimonials</a></li>
-            <li ><a href="#contact">Contact</a></li>
-           {user ? (
-             <li onClick={handleLogout}  style={{ cursor: 'pointer' }}><a className='signBtn'>Sign Out</a></li>
-           ) : (
-            <li onClick={handleGoogleSignIn} style={{ cursor: 'pointer' }}><a className='signBtn'>Sign In</a></li>
-           )}
-           {user && <img src={user.photoURL} alt={user.displayName} className='signin' />}
-           <li className='change_mode'>
-              {mode === 'light' ? (
-                <MdLightMode
-                className='mode_icon'
-                onClick={()=>changeMode('dark')}
-               />
-              ) : (
-                <MdOutlineLightMode 
-                className='mode_icon dark_mode'
-                onClick={()=>changeMode('light')}
-               />
-              )}
+          {menuOpen ? (
+            <VscEyeClosed
+              onClick={() => setMenuOpen(!menuOpen)}
+              className="menu_open"
+            />
+          ) : (
+            <RiMenuAddLine
+              onClick={() => setMenuOpen(!menuOpen)}
+              className="menu_open"
+            />
+          )}
+        </div>
+        <ul
+          className={menuOpen ? "nav_links show" : "nav_links"}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          {user ? (
+            <li onClick={handleLogout} style={{ cursor: "pointer" }}>
+              <a className="signBtn">Sign Out</a>
             </li>
+          ) : (
+            <li onClick={handleGoogleSignIn} style={{ cursor: "pointer" }}>
+              <a className="signBtn">Sign In</a>
+            </li>
+          )}
+          {user && (
+            <img
+              src={user.photoURL}
+              alt={user.displayName}
+              className="signin"
+            />
+          )}
+          <li className="change_mode">
+            {mode === "light" ? (
+              <MdLightMode
+                className="mode_icon"
+                onClick={() => changeMode("dark")}
+              />
+            ) : (
+              <MdOutlineLightMode
+                className="mode_icon dark_mode"
+                onClick={() => changeMode("light")}
+              />
+            )}
+          </li>
         </ul>
-     </div>
-   </nav>
-    )
+      </div>
+    </nav>
+  );
 }
